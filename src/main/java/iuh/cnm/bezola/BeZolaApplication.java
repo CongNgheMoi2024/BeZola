4b26ac3864048c53ea6916ae537360ac8ccee4e8
HoaiAn0906 <phoaianvlogs@gmail.com>
2024-01-06 08:57:29 +0700

    init

---
+++ src/main/java/iuh/cnm/bezola/BeZolaApplication.java
@@ -0,0 +1,13 @@
+package iuh.cnm.bezola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeZolaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeZolaApplication.class, args);
+    }
+
+}
